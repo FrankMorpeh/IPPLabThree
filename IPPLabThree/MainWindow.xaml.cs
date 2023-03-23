175438d8d695118e4cac0c55b3c6d61c3e1a644e
Bogdan Bakhmatskyi <bakhmatskyibogdan@gmail.com>
2023-03-23 20:38:38 +0200

    Add project files.

---
+++ IPPLabThree/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using IPPLabThree.Pages;
+using System.Windows;
+
+namespace IPPLabThree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new MainPage(frame);
+        }
+    }
+}
---
+++ IPPLabThree/Pages/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace IPPLabThree.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private Frame frame;
+        public MainPage(Frame frame)
+        {
+            InitializeComponent();
+            this.frame = frame;
+        }
+        private void Pads_Clicked(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new ControllerPage();
+        }
+        private void Player_Clicked(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new GamePage();
+        }
+    }
+}
